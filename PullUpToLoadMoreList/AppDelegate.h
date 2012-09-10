5f03a0db7b46af02a79d38302f80edbb4be234c1
xuzhongzhou <xuzhongzhou@foxmail.com>
2012-09-10 15:06:01 +0800

    Initial Commit

---
+++ PullUpToLoadMoreList/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PullUpToLoadMoreList
+//
+//  Created by 许 忠洲 on 12-9-10.
+//  Copyright (c) 2012年 eshore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ PullUpToLoadMoreList/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PullUpToLoadMoreList
+//
+//  Created by 许 忠洲 on 12-9-10.
+//  Copyright (c) 2012年 eshore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
